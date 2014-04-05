19056ddb5d5351b3bf92aa08da72e21f6d8ac18e
A-L-D <andrew@andrewdemarsh.ca>
2014-04-05 16:35:08 -0400

    beginning of search function has formed however i am currently frustrated with the search function's effectivness as it does not seem to notice all 4 instances of the search term in the database finding only 3.

--- LibraryDatabase.java
+++ LibraryDatabase.java
@@ -2,15 +2,26 @@ import javax.swing.*;
 import javax.swing.border.TitledBorder;
 
 import java.util.*;
+import java.util.List;
+import java.util.regex.PatternSyntaxException;
 import java.awt.*;
 import java.awt.event.*;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+
 
 
 
 public class LibraryDatabase extends JFrame implements ActionListener{
 	JTextField txtSearchBy;
 	JLabel lblSearchBy;
-	JComboBox<String> comboSearchBy;
+	JComboBox<String> comboSearchBy, searchResults;
 	JButton btnAddBook, btnModifyBook, btnListAll;
 	JTextArea textAreaList;
 	public LibraryDatabase() {
@@ -25,21 +36,26 @@ public class LibraryDatabase extends JFrame implements ActionListener{
 		JComboBox comboSearchBy = new JComboBox();
 		comboSearchBy.setBounds(124, 10, 150, 20);
 		getContentPane().add(comboSearchBy);
+		comboSearchBy.addItem("Title");
+		comboSearchBy.addItem("Author");
+		comboSearchBy.addItem("ISBN");
+		comboSearchBy.addItem("Date (In yyyy/mm/dd format");
+		comboSearchBy.addItem("Rating Greater than");
+		comboSearchBy.addItem("Rating Less than");
 		
 		JButton btnSearch = new JButton("Search");
 		btnSearch.setBounds(10, 67, 264, 23);
 		getContentPane().add(btnSearch);
 		
-		JTextField txtSearchBy = new JTextField();
+		final JTextField txtSearchBy = new JTextField();
 		txtSearchBy.setText("Enter Search Value Here:");
 		txtSearchBy.setBounds(10, 36, 264, 20);
 		getContentPane().add(txtSearchBy);
 		txtSearchBy.setColumns(10);
 		
-		JTextArea textAreaList = new JTextArea();
-		textAreaList.setFont(new Font("Monospaced", Font.PLAIN, 10));
-		textAreaList.setBounds(0, 101, 284, 215);
-		getContentPane().add(textAreaList);
+		JComboBox searchResults = new JComboBox();
+		searchResults.setBounds(0, 101, 284, 20);
+		getContentPane().add(searchResults);
 		
 		JButton btnAddBook = new JButton("Add Book");
 		btnAddBook.setFont(new Font("Tahoma", Font.BOLD, 8));
@@ -56,6 +72,46 @@ public class LibraryDatabase extends JFrame implements ActionListener{
 		btnListAll.setFont(new Font("Tahoma", Font.BOLD, 8));
 		btnListAll.setBounds(185, 327, 89, 23);
 		getContentPane().add(btnListAll);
+		
+		btnSearch.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				Scanner scanner = null;
+				try {
+					scanner = new Scanner(new File ("database.dat"));
+				} catch (FileNotFoundException e1) {
+					e1.printStackTrace();
+				}	
+					// Open the file database.dat as a buffered reader
+					try {
+						BufferedReader bf = new BufferedReader(new FileReader("database.dat"));
+					} catch (FileNotFoundException e) {
+						e.printStackTrace();
+					}
+					
+					// Start a line count and declare a string to hold our current line.
+					int linecount = 0;
+					String line;
+					
+					// Let the user know what we are searching for
+					
+					 // Loop through each line, storing the line into our line variable.
+					 List<String> list=new ArrayList<>();
+					 StringBuilder buffer = new StringBuilder();
+					 while(scanner.hasNextLine()){
+					     list.add(scanner.nextLine());
+					     buffer.append(list);
+				         buffer.append('\n');
+					     String Buffer = buffer.toString(); 
+				    	 String[] splitArray = Buffer.split("\\s+");
+				    	 String search = txtSearchBy.getText();
+				    	 //TODO: cant seem to figure out why only 3 values are being found at the moment instead of 4
+					    	 for(int i = 0; i < splitArray.length; i++) {
+					    		 if(search.equals(splitArray[i])){
+							         System.out.println(buffer);
+					    		 }
+					    	 }
+					 }
+			}});
 		
 	}
 
@@ -125,31 +181,31 @@ public class LibraryDatabase extends JFrame implements ActionListener{
 			getContentPane().add(genreP);
 			genreP.setLayout(null);
 			
-			JCheckBox chkSci = new JCheckBox("Sci - Fi");
+			final JCheckBox chkSci = new JCheckBox("Sci - Fi");
 			chkSci.setBounds(23, 22, 97, 23);
 			genreP.add(chkSci);
 			
-			JCheckBox chkFant = new JCheckBox("Fantasy");
+			final JCheckBox chkFant = new JCheckBox("Fantasy");
 			chkFant.setBounds(23, 48, 97, 23);
 			genreP.add(chkFant);
 			
-			JCheckBox chkRomance = new JCheckBox("Romance");
+			final JCheckBox chkRomance = new JCheckBox("Romance");
 			chkRomance.setBounds(23, 74, 97, 23);
 			genreP.add(chkRomance);
 			
-			JCheckBox chkAction = new JCheckBox("Action");
+			final JCheckBox chkAction = new JCheckBox("Action");
 			chkAction.setBounds(23, 100, 97, 23);
 			genreP.add(chkAction);
 			
-			JCheckBox chkThriller = new JCheckBox("Thriller");
+			final JCheckBox chkThriller = new JCheckBox("Thriller");
 			chkThriller.setBounds(23, 126, 97, 23);
 			genreP.add(chkThriller);
 			
-			JCheckBox chkHorror = new JCheckBox("Horror");
+			final JCheckBox chkHorror = new JCheckBox("Horror");
 			chkHorror.setBounds(23, 152, 97, 23);
 			genreP.add(chkHorror);
 			
-			JSlider sRating = new JSlider();
+			final JSlider sRating = new JSlider();
 			sRating.setBounds(118, 280, 200, 26);
 			getContentPane().add(sRating);
 			
@@ -175,6 +231,18 @@ public class LibraryDatabase extends JFrame implements ActionListener{
 			lblRating.setBounds(118, 226, 95, 20);
 			getContentPane().add(lblRating);
 			
+			btnSave.addActionListener(new ActionListener() {
+				public void actionPerformed(ActionEvent arg0) {
+					PrintWriter fileWriter = null;
+					try {
+						fileWriter = new PrintWriter(new BufferedWriter(new FileWriter("database.dat", true)));
+					} catch (IOException e) {
+						e.printStackTrace();
+					}
+					fileWriter.println( txtTitle.getText()+" , "+txtaName.getText()+" , "+txtISBN.getText()+" , "+txtDate.getText()+" , "+String.valueOf(chkSci.isSelected())+" , "+String.valueOf(chkFant.isSelected())+" , "+String.valueOf(chkRomance.isSelected())+" , "+String.valueOf(chkAction.isSelected())+" , "+String.valueOf(chkThriller.isSelected())+" , "+String.valueOf(chkHorror.isSelected())+" , "+sRating.getValue());
+					fileWriter.close();
+				}
+			});
 		}
 		@Override
 		public void actionPerformed(ActionEvent arg0) {
